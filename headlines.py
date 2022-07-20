599a5405f90306e93a1cc7bd2a0e598ff4a04360
TeenAsher <106741447+TeenAsher@users.noreply.github.com>
2022-07-20 12:09:08 +0300

    Added city search

--- headlines.py
+++ headlines.py
@@ -9,23 +9,42 @@ app = Flask(__name__)
 
 RSS_FEEDS = {
     'cnn': 'http://rss.cnn.com/rss/edition.rss',
-    'fox': 'http://feeds.foxnews.com/foxnews/latest'
+    'fox': 'http://feeds.foxnews.com/foxnews/latest',
+    'оон': 'https://news.un.org/feed/subscribe/ru/news/all/rss.xml',
+    'npr': 'https://rss2.feedspot.com/https://www.npr.org/sections/national/',
+    'sky': 'https://feeds.skynews.com/feeds/rss/world.xml'
+}
+
+DEFAULTS = {
+    'publication': 'cnn',
+    'city': 'Saint-Petersburg'
 }
 
 @app.route('/')
-def get_news():
-    query = flask.request.args.get('publication')
+def home():
+    publication = flask.request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    city = flask.request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template('home.html', articles=articles, weather=weather)
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = 'fox'
+        publication = DEFAULTS['publication']
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    weather = get_weather()
-    return render_template('home.html', articles=feed['entries'], weather=weather)
+    return feed['entries']
 
-def get_weather():
-    api_url = 'http://api.weatherapi.com/v1/current.json?key=c60b3b20a7f14529866163025221307&q=London&aqi=no'
-    data = request.urlopen(api_url).read()
+def get_weather(query):
+    query = parse.quote(query)
+    api_url = f'http://api.weatherapi.com/v1/current.json?key=c60b3b20a7f14529866163025221307&q={query}&aqi=no'
+    url = api_url.format(query)
+    data = request.urlopen(url).read()
     parsed = json.loads(data)
     weather = None
     if parsed.get('current'):
