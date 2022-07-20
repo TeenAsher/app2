f5f74113f8fb3227e8977519ab17fb834c1fb63e
TeenAsher <106741447+TeenAsher@users.noreply.github.com>
2022-07-20 14:19:16 +0300

    Added cookies

--- headlines.py
+++ headlines.py
@@ -1,8 +1,10 @@
+import datetime
 import feedparser
+import flask
 import json
 from flask import Flask
+from flask import make_response
 from flask import render_template
-import flask
 from urllib import parse, request
 
 app = Flask(__name__)
@@ -24,26 +26,32 @@ DEFAULTS = {
 
 CURRENCY_URL = 'https://openexchangerates.org//api/latest.json?app_id=0b0c3a8e4e4243d48c89316d6cba8c84'
 
+def get_value_with_fallback(key):
+    if flask.request.args.get(key):
+        return flask.request.args.get(key)
+    if flask.request.cookies.get(key):
+        return flask.request.cookies.get(key)
+    return DEFAULTS[key]
+
 @app.route('/')
 def home():
-    publication = flask.request.args.get('publication')
-    if not publication:
-        publication = DEFAULTS['publication']
+    publication = get_value_with_fallback('publication')
     articles = get_news(publication)
-    city = flask.request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback('city')
     weather = get_weather(city)
-    currency_from = flask.request.args.get('currency_from')
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = flask.request.args.get('currency_to')
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback('currency_from')
+    currency_to = get_value_with_fallback('currency_to')
     rate, currencies = get_rates(currency_from, currency_to)
-    return render_template('home.html', articles=articles, weather=weather,
-                           currency_from=currency_from, currency_to=currency_to,
-                           rate=rate, currencies=sorted(currencies))
+    response = make_response(
+        render_template('home.html', articles=articles, weather=weather,
+                        currency_from=currency_from, currency_to=currency_to,
+                        rate=rate, currencies=sorted(currencies)))
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie('publication', publication, expires=expires)
+    response.set_cookie('city', city, expires=expires)
+    response.set_cookie('currency_from', currency_from, expires=expires)
+    response.set_cookie('currency_to', currency_to, expires=expires)
+    return response
 
 def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
